62e87c2961029c98b35734091b4e2adce25b5ca8
James Ruan <ruanbeihong@gmail.com>
2018-03-29 13:29:48 +0800

    use rt8192cu

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -3,3 +3,5 @@
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
+install -v -m 600 files/bl-rtl8192cu.conf	"${ROOTFS_DIR}/etc/modprobe.d/"
+install -v -m 600 files/bl-8192cu.conf		"${ROOTFS_DIR}/etc/modprobe.d/"
